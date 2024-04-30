04460c3ab6d37632478ea492c3059fa87611725b
BottleMoon <gwonbj.dev@gmail.com>
2024-05-01 06:28:41 +0900

    feat: entity, dto

---
+++ src/main/java/flab/optimization/dto/SanggaDto.java
@@ -0,0 +1,13 @@
+package flab.optimization.dto;
+
+import lombok.Data;
+
+@Data
+public class SanggaDto {
+    private String name;
+    private String jibun_address;
+    private String doro_address;
+    private String bigClassificationName;
+    private String mediumClassificationName;
+    private String smallClassificationName;
+}
---
+++ src/main/java/flab/optimization/entity/SmallClassificationName.java
@@ -0,0 +1,14 @@
+package flab.optimization.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class SmallClassificationName {
+    @Id
+    private Long id;
+
+    private String name;
+}
